8d95fc1cfa1469b474e85f5619db34459e4dec06
Dylan Lamont <dylanlamont3840@gmail.com>
2022-03-16 16:30:46 +1100

    Implements working garden list placeholder

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,21 +1,41 @@
-import { StyleSheet } from "react-native";
-
+import { FlatList, StyleSheet } from "react-native";
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
-
+import { useAppSelector, useAppDispatch } from "../store";
 export default function TabOneScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
+  const garden = useAppSelector((state) => state.garden);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab 1</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+      <Text style={styles.title}>Garden Beds</Text>
+      <FlatList
+        data={garden.beds}
+        renderItem={({ item }) => (
+          <View key={item.id}>
+            <Text style={styles.subTitle}>{item.name}</Text>
+            {item.plants && (
+              <FlatList
+                data={item.plants}
+                renderItem={({ item }) => (
+                  <View style={styles.plantContainer} key={item.id}>
+                    <Text style={{ color: "green" }}>{item.name}</Text>
+                    {item.notes && <Text>Notes: {item.notes}</Text>}
+
+                    <View
+                      style={styles.separator}
+                      lightColor="#eee"
+                      darkColor="rgba(255,255,255,0.1)"
+                    />
+                  </View>
+                )}
+              />
+            )}
+          </View>
+        )}
       />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
@@ -26,12 +46,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  plantContainer: {
+    paddingLeft: 15,
+  },
   title: {
     fontSize: 20,
     fontWeight: "bold",
   },
+  subTitle: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
   separator: {
-    marginVertical: 30,
+    marginVertical: 5,
     height: 1,
     width: "80%",
   },
