76c20e1064ca2a302ae9c824c7afef2125a71a61
Dylan Lamont <38599829+didley@users.noreply.github.com>
2022-04-09 03:04:14 +1000

    Implements veggieList to add veggie modal and add buttons

--- screens/AddVeggieModalScreen.tsx
+++ screens/AddVeggieModalScreen.tsx
@@ -1,22 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
+import { VeggieList } from "../components/VeggieList";
+import { mockData } from "../services/mockData";
+import { VeggieInfo } from "../services/types";
 
 export const AddVeggieModalScreen = () => {
+  const { veggieInfos } = mockData;
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+      <VeggieList
+        veggies={veggieInfos}
+        addHandler={() => console.log("TODO add handler")}
       />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 };
