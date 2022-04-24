5411ddb8031da191c12211e8d94fcbc9daa37c32
Dylan Lamont <38599829+didley@users.noreply.github.com>
2022-04-24 19:16:50 +1000

    Fix - PlatformColor was causing crash on web and android, i've just set the color to dodgerblue. The extra code to have PlatformColor working is not worth it

--- components/shared/SortBtn.tsx
+++ components/shared/SortBtn.tsx
@@ -1,6 +1,6 @@
 import { Octicons } from "@expo/vector-icons";
 import { ComponentProps } from "react";
-import { PlatformColor } from "react-native";
+import { Platform, PlatformColor } from "react-native";
 
 type Props = { descending: boolean } & Omit<
   ComponentProps<typeof Octicons>,
@@ -11,7 +11,7 @@ export const SortBtn = ({ descending, ...rest }: Props) => (
   <Octicons
     name={descending ? "sort-desc" : "sort-asc"}
     size={24}
-    color={PlatformColor("linkColor")}
+    color={"dodgerblue"}
     {...rest}
   />
 );
