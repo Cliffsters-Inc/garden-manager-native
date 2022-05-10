1030d605f00ff52097bd1603670d593b86073a18
Dylan Lamont <38599829+didley@users.noreply.github.com>
2022-05-10 19:07:36 +1000

    Implements more tests

--- screens/__tests__/BedTabScreen.test.tsx
+++ screens/__tests__/BedTabScreen.test.tsx
@@ -1,7 +1,46 @@
 import { renderApp, fireEvent, waitFor } from "../../testing/test-utils";
 
 describe("<BedsTabScreen />", () => {
-  it.todo("renders veggies");
+  it("can add a veggie", async () => {
+    const {
+      findByText,
+      getByPlaceholderText,
+      getAllByText,
+      getByText,
+      getByTestId,
+    } = renderApp();
 
-  it.todo("can add a veggie");
+    // creates new garden
+    const addGardenBtn = await findByText(/add garden/i);
+    fireEvent.press(addGardenBtn);
+    const gardenNameField = getByPlaceholderText(/new garden name/i);
+    fireEvent.changeText(gardenNameField, "TestGarden123");
+    const createGardenDoneBtn = getAllByText(/done/i)[0];
+    fireEvent.press(createGardenDoneBtn);
+    await waitFor(() => getByText("TestGarden123"));
+
+    // navigates to beds screen
+    const newGarden = await findByText("TestGarden123");
+    fireEvent.press(newGarden);
+
+    // creates new bed
+    const addBedBtn = await findByText(/add bed/i);
+    fireEvent.press(addBedBtn);
+    const bedNameField = getByPlaceholderText(/new bed name/i);
+    fireEvent.changeText(bedNameField, "TestBed123");
+    const createBedDoneBtn = getAllByText(/done/i)[0];
+    fireEvent.press(createBedDoneBtn);
+    const newBed = await waitFor(() => getByText("TestBed123"));
+
+    // navigates to bed
+    fireEvent.press(newBed);
+
+    // adds new veggie
+    const newVeggieBtn = await findByText(/add veggie/i);
+    fireEvent.press(newVeggieBtn);
+    const eggplantAddBtn = getByTestId(/eggplant-add-btn/i);
+    fireEvent.press(eggplantAddBtn);
+    const eggplant = getByText(/^eggplant$/i);
+    expect(eggplant).toBeDefined();
+  });
 });
--- screens/__tests__/EditVeggieLogModal.test.tsx
+++
@@ -1,7 +0,0 @@
-import { renderApp, fireEvent, waitFor } from "../../testing/test-utils";
-
-describe("<EditVeggieLogModal />", () => {
-  it.todo("can edit a log");
-
-  it.todo("can delete a log");
-});
