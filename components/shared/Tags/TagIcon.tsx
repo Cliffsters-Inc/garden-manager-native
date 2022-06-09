975d69a429751febeb5b6d343ee15bfcd4bb190a
Uken81 <64936770+Uken81@users.noreply.github.com>
2022-06-09 10:04:55 +1000

    Create TagIcon component.

---
+++ components/shared/Tags/TagIcon.tsx
@@ -0,0 +1,55 @@
+import {
+  FontAwesome5,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import { View } from "../../Themed";
+
+export type Icon = {
+  id: string;
+  name: string;
+  icon: any;
+};
+
+type Props = {
+  iconColor: string | undefined;
+  selectedIcon: string | undefined;
+};
+
+export const TagIcon = ({ iconColor, selectedIcon }: Props) => {
+  const iconList: Icon[] = [
+    {
+      id: "pest",
+      name: "pest",
+      icon: <Ionicons name="ios-bug-outline" size={20} color={iconColor} />,
+    },
+    {
+      id: "disease",
+      name: "disease",
+      icon: <FontAwesome5 name="virus" size={20} color={iconColor} />,
+    },
+    {
+      id: "seed",
+      name: "seed",
+      icon: (
+        <MaterialCommunityIcons
+          name="seed-outline"
+          size={20}
+          color={iconColor}
+        />
+      ),
+    },
+    {
+      id: "seedling",
+      name: "seedling",
+      icon: <FontAwesome5 name="seedling" size={20} color="black" />,
+    },
+  ];
+
+  // const selectedIconName = "seed";
+  const Icon = iconList.find((icon) => icon.name === selectedIcon);
+  // const selectedIconName = "seed";
+  // const Icon = iconList.find((icon) => icon.name === selectedIconName);
+
+  return <View>{Icon?.icon}</View>;
+};
