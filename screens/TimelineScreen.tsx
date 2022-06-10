a90d4c40266dd407de961cb28e70c3721ec95422
Uken81 <64936770+Uken81@users.noreply.github.com>
2022-06-10 15:41:42 +1000

    Implement and test global timeline in the TimelineScreen component.

--- screens/TimelineScreen.tsx
+++ screens/TimelineScreen.tsx
@@ -1,4 +1,7 @@
-import { Text, View } from "../components/Themed";
+import { format } from "date-fns";
+import { StyleSheet } from "react-native";
+import Timeline from "react-native-timeline-flatlist";
+import { View } from "../components/Themed";
 import { gardenSelectors } from "../services/garden/garden.selectors";
 import { useAppSelector } from "../store";
 
@@ -9,9 +12,37 @@ export const TimelineScreen = () => {
 
   console.log("logs", logs);
 
+  const data = logs?.map((value) => ({
+    time: format(new Date(value.date), "d MMM yy"),
+    title: value.id,
+    description: value.notes,
+  }));
+
+  console.log(
+    "data: ",
+    logs.forEach((e) => {
+      console.log("e: ", e.notes);
+    })
+  );
+
   return (
-    <View>
-      <Text>Timeline</Text>
+    <View style={styles.container}>
+      <Timeline data={data} style={styles.list} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingTop: 65,
+    backgroundColor: "white",
+    width: "100%",
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
+    width: "100%",
+  },
+});
