28e2bf4102f21fa91c21b3dc5a2cc4dcc405f36c
Uken81 <64936770+Uken81@users.noreply.github.com>
2022-06-15 09:24:57 +1000

    Add functionality to display a tag icon for logs in the timeline. Create test data logs.

--- screens/TimelineScreen.tsx
+++ screens/TimelineScreen.tsx
@@ -1,7 +1,9 @@
+import { FontAwesome5, Ionicons } from "@expo/vector-icons";
 import { format } from "date-fns";
 import { StyleSheet } from "react-native";
 import Timeline from "react-native-timeline-flatlist";
 import { View } from "../components/Themed";
+import { FindIcon } from "../components/TimelineIcon";
 import { gardenSelectors } from "../services/garden/garden.selectors";
 import { useAppSelector } from "../store";
 
@@ -11,23 +13,54 @@ export const TimelineScreen = () => {
   );
 
   console.log("logs", logs);
+  logs.forEach((log: any) => {
+    console.log(log.payloadTags[0].tagColor);
+  });
+
+  const assignTag = (
+    tagName: string | undefined,
+    tagColor: string | undefined
+  ) => {
+    switch (tagName) {
+      case "pests":
+        return <Ionicons name="ios-bug-outline" size={20} color={tagColor} />;
+      case "disease":
+        return <FontAwesome5 name="virus" size={20} color={tagColor} />;
+    }
+  };
 
   const data = logs?.map((value) => ({
     time: format(new Date(value.date), "d MMM yy"),
-    title: value.id,
+    // title: value.id,
     description: value.notes,
+    icon: assignTag(
+      //*********changing below from value.payloadTags[0].tagLabel caused current error*********
+      value.payloadTags[0].tagLabel,
+      value.payloadTags[0].tagColor
+    ),
   }));
-
-  console.log(
-    "data: ",
-    logs.forEach((e) => {
-      console.log("e: ", e.notes);
-    })
-  );
 
   return (
     <View style={styles.container}>
-      <Timeline data={data} style={styles.list} />
+      <Timeline
+        data={data}
+        style={styles.list}
+        innerCircle={"icon"}
+        circleColor={"white"}
+        descriptionStyle={styles.description}
+        detailContainerStyle={{
+          backgroundColor: "rgba(126, 208, 252, 0.5)",
+          maxWidth: 180,
+          minHeight: 40,
+          borderColor: "black",
+          borderStyle: "solid",
+          borderWidth: 2,
+          borderRadius: 20,
+        }}
+        // separator={false}
+        // columnFormat="two-column"
+      />
+      <FindIcon selectedIcon={"pest"} />
     </View>
   );
 };
@@ -44,5 +77,15 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20,
     width: "100%",
+  },
+  description: {
+    backgroundColor: "rgba(126, 208, 252, 0.5)",
+    // maxWidth: 180,
+    // minHeight: 40,
+    // borderColor: "black",
+    // borderStyle: "solid",
+    // borderWidth: 2,
+    // borderRadius: 20,
+    padding: 5,
   },
 });
